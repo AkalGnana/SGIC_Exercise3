30fc4ee40ca5066b9633d05f46472ab7ec7c7d4f
Akalya gnanasekaram <akalya.gnanasekaram@gmail.com>
2022-09-02 17:19:46 +0530

    Utility main class

---
+++ Q4_main.java
@@ -0,0 +1,15 @@
+package Exercise_3;
+
+public class Q4_main {
+
+	public static void main(String[] args) {
+//System.out.println(Qu_4.add(10));
+
+		System.out.println("Removezero : " + Qu_4.removezero("00012458"));
+		System.out.println("String is nullorempty : " + Qu_4.isNullorEmpty(""));
+		System.out.println("String is AlphaNumeric : " + Qu_4.AlphaNumeric("5458455ghfft"));
+		System.out.println("Convert String to date : " + Qu_4.Stringdtodate("05/05/2022"));
+
+	}
+
+}
