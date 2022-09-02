74c4fb5921e263f1f73716d313d971b7b9d3ac7f
Akalya gnanasekaram <akalya.gnanasekaram@gmail.com>
2022-09-02 17:21:51 +0530

    Junit test class

---
+++ JunitTesting.java
@@ -0,0 +1,47 @@
+package Exercise_3;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JunitTesting {
+	// (A) String covert to date
+	public static Date Stringdtodate(String dates) {
+		try {
+			SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+			Date date = format.parse(dates);
+			return date;
+
+		} catch (Exception e) {
+			return null;
+
+		}
+
+	}
+
+	// (B) isNullorEmpty
+	public static boolean isNullorEmpty(String st) {
+		if (st == null || st.isEmpty()) {
+			return false;
+		}
+		return true;
+
+	}
+
+	// (C)RemoveZero
+	public static String removezero(String st) {
+		String str = "^0+(?!$)";
+		st = st.replaceAll(str, "");
+		return st;
+	}
+
+	// (D)AlphaNumeric
+	public static boolean AlphaNumeric(String st) {
+		String regex = "^[a-zA-Z0-9]+$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(st);
+		return matcher.matches();
+
+	}
+}
